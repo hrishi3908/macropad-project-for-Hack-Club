3f54f9e8ed9789996a573221da3302be06512396
_Hrishi_3908 <hrishikeshkumar3908@gmail.com>
2025-12-18 00:18:16 +0530

    Add initial firmware structure for macropad

---
+++ firmware/main.ino
@@ -0,0 +1,10 @@
+// Macropad firmware (placeholder)
+// Will be updated as hardware development progresses
+
+void setup() {
+  // initialize HID and OLED
+}
+
+void loop() {
+  // handle key presses
+}
